f596457dfbae224c576f88be29672e229134c122
PavelEgorov <jonnycomein@gmail.com>
2020-10-22 20:22:50 +0300

    Урок 2. Добавлен адаптер. Метеостанция

--- app/src/main/java/com/first/polygonteam/interview/lesson2/MeteoStation.java
+++ app/src/main/java/com/first/polygonteam/interview/lesson2/MeteoStation.java
@@ -1,6 +1,8 @@
 package com.first.polygonteam.interview.lesson2;
 
 public class MeteoStation{
+    private static Integer startIdST500 = 1000;
+
     public static void main(String[] args){
         MeteoStore meteoDb = new MeteoStore();
 
@@ -8,5 +10,8 @@ public class MeteoStation{
         meteoDb.save(ms200_1);
 
         // Здесь надо вызвать метод getData у класса ST500Info. Полученные данные отправить в метод save объекта meteoDb
+        ST500Info sensor = new ST500Info();
+        ST500Adapter adapter = new ST500Adapter(sensor.getData(), startIdST500);
+        meteoDb.save(adapter);
     }
 }
